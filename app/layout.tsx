d470ca5e98872e595ab4416138c5c2b8b6f0c22d
Debangshu Dey <deydebangshu51@gmail.com>
2025-03-08 23:18:53 +0530

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SystemR",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "SystemR is a web development agency",
 };
 
 export default function RootLayout({
