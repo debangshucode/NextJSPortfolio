151409661b2ea1f06df656e3d59f781d41fbadfa
somuthehunter <100991071+somuthehunter@users.noreply.github.com>
2025-03-11 22:53:49 +0530

    smooth

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // ✅ Add this here
+  // output: "export", // ✅ Add this here
 };
 
 export default withSentryConfig(
