66ae3092dc13c900dff28a6b50ec284442b4c72a
somuthehunter <100991071+somuthehunter@users.noreply.github.com>
2025-03-11 11:15:07 +0530

    animation updated with the navbar size

--- components/ui/CounterCard.tsx
+++ components/ui/CounterCard.tsx
@@ -18,7 +18,7 @@ const CounterCard: React.FC<CounterCardProps> = ({ count, label }) => {
       ref={ref}
       initial={{ opacity: 0, x: 50 }}
       animate={inView ? { opacity: 1, x: 0 } : {}}
-      transition={{ duration: 0.6, ease: "easeOut" }}
+      transition={{ duration: 0.6, ease: "easeInOut" }}
       className="text-center p-4 sm:p-5 rounded-lg backdrop-blur-lg bg-white/10 border border-white/20 shadow-md w-full max-w-xs sm:max-w-sm md:max-w-52 lg:max-w-60"
     >
       <h2 className="text-3xl sm:text-4xl font-bold text-white">
