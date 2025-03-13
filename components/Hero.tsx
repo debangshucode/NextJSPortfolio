d663e86365c266bd7a658d3e2358b0e7b311a75c
somuthehunter <100991071+somuthehunter@users.noreply.github.com>
2025-03-13 21:30:28 +0530

    text generate effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -95,10 +95,14 @@ const Hero = () => {
           <p className="uppercase tracking-widest text-xs text-center text-red-100 max-w-80">
             Dynamic Web Magic with SystemR
           </p>
-          <TextGenerateEffect
+          {/* <TextGenerateEffect
             words="Transforming Ideas Into Seamless Digital Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-          />
+          /> */}
+
+          <h1 className="text-center text-[40px] md:text-5xl lg:text-6xl">
+            Transforming Ideas Into Seamless Digital Experiences
+          </h1>
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             System<span className="text-red-500">R</span> – Delivering
             cutting-edge IT solutions with Next.js and modern web technologies.
