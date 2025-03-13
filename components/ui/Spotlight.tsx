ef51910366788e9b0074d74a2fd7947d89758e21
Debangshu Dey <deydebangshu51@gmail.com>
2025-03-14 02:22:56 +0530

    spotlight

--- components/ui/Spotlight.tsx
+++ components/ui/Spotlight.tsx
@@ -10,15 +10,15 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1] h-[100%] w-[90%] lg:w-[84%] opacity-0",
+        "spotlight pointer-events-none absolute z-[1] h-[100%] w-[90%] lg:w-[84%] opacity-0",
         className
       )}
-      style={{ willChange: "transform, opacity" }} // Add this
+      style={{ willChange: "transform, opacity" }}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 3787 2842"
       fill="none"
     >
-      <g filter="url(#filter)">
+      <g>
         <ellipse
           cx="1924.71"
           cy="273.501"
@@ -29,29 +29,6 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           fillOpacity="0.21"
         ></ellipse>
       </g>
-      <defs>
-        <filter
-          id="filter"
-          x="0.860352"
-          y="0.838989"
-          width="3785.16"
-          height="2840.26"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          ></feBlend>
-          <feGaussianBlur
-            stdDeviation="40"
-            result="effect1_foregroundBlur_1065_8"
-          ></feGaussianBlur>
-        </filter>
-      </defs>
     </svg>
   );
 };
