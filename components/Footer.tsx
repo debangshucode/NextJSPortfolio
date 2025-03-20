dcb0118897ec036c491817107739812fad28f1f3
somuthehunter <100991071+somuthehunter@users.noreply.github.com>
2025-03-20 22:09:59 +0530

    updated social media icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 import ContactUS from "./contactUS"; // Import the Contact Form
-
+import Socialmedia from "./socialmedia";
 const Footer = () => {
   return (
     <footer className="w-full pt-10 pb-10 relative" id="contact">
@@ -38,21 +38,13 @@ const Footer = () => {
         <ContactUS />
       </div>
 
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col gap-4 justify-between items-center">
+        <div className="flex items-center md:gap-3 gap-6">
+          <Socialmedia />
+        </div>
         <p className="md:text-base text-sm md:font-normal font-light">
           Created by DreamGuys
         </p>
-
-        <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
-          ))}
-        </div>
       </div>
     </footer>
   );
