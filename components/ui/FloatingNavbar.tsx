1d11da519cff45aa11695ad9c4da394da7bd773e
Debangshu Dey <deydebangshu51@gmail.com>
2025-03-21 21:05:13 +0530

    fixing conflicts

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -4,7 +4,6 @@ import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { AnimatePresence } from "framer-motion";
-import { AnimatePresence } from "framer-motion";
 export const FloatingNav = ({
   navItems,
   className,
